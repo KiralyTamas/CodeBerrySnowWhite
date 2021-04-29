728bf319628f1db4732827af88f3abfefe00032f
KiralyTamas <tomaszpest@gmail.com>
2021-04-29 20:16:57 +0200

    Nyuszi gyarapodás

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var dwarfCounter = 1;
-while(dwarfCounter<=7){
-  console.log('A(z) '+dwarfCounter+'. törpe a bányában.');
-  dwarfCounter++;
+for (let monthCounter = 1, rabbitPopulation = 30; monthCounter <= 5; monthCounter++, rabbitPopulation = rabbitPopulation * 2) {
+  console.log('A Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
+};
+for (let monthCounter = 6, rabbitPopulation = 960; monthCounter <= 12; monthCounter++, rabbitPopulation = rabbitPopulation * 3) {
+  console.log('A Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
 }
