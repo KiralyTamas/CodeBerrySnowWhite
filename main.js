43d4403f94e905e12b5e68e66a975968ce6e39b9
KiralyTamas <tomaszpest@gmail.com>
2021-04-29 20:54:59 +0200

    Nyuszik szaporodnak.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 for (let monthCounter = 1, rabbitPopulation = 30; monthCounter <= 5; monthCounter++, rabbitPopulation = rabbitPopulation * 2) {
-  console.log('A Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
+  console.log('Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
 };
 for (let monthCounter = 6, rabbitPopulation = 960; monthCounter <= 12; monthCounter++, rabbitPopulation = rabbitPopulation * 3) {
   console.log('A Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
-}
+};
