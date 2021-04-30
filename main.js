42081ac8a1b5cb092a38fb19be228046f83a34c4
KiralyTamas <tomaszpest@gmail.com>
2021-04-30 18:22:00 +0200

    önálló feladat kész.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-for (let monthCounter = 1, rabbitPopulation = 30; monthCounter <= 5; monthCounter++, rabbitPopulation = rabbitPopulation * 2) {
-  console.log('Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
-};
-for (let monthCounter = 6, rabbitPopulation = 960; monthCounter <= 12; monthCounter++, rabbitPopulation = rabbitPopulation * 3) {
-  console.log('A Skylark-völgy nyulainak száma ' + rabbitPopulation + ' a(z) ' + monthCounter + '. hónap végén.')
-};
+var totalBuses = 10;
+var busCounter = 1;
+var workingBuses = 7;
+for (busCounter; busCounter <= workingBuses; busCounter++) {
+  console.log(busCounter + '. busz üzemkész, soförre vár.')
+}
+for (busCounter; busCounter <= totalBuses; busCounter++) {
+  console.log(busCounter + '. busz üzemen kívül van, szervizre vár.')
+}
