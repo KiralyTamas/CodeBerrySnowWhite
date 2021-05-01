5031509eb327b2cc532ec01995da8dd402bca70f
KiralyTamas <tomaszpest@gmail.com>
2021-05-01 17:59:29 +0200

    Revert ".js báhívása html-be folyamatban."
    This reverts commit 92493bfb9a8647b536b9317e6e96d205b56111b4.

--- dwarf.js
+++ dwarf.js
@@ -1,11 +1,9 @@
 var totalDwarf = 10;
 var dwarfCounter = 1;
 var workingDwarfs = 7;
-function torpe() {
-  for (dwarfCounter; dwarfCounter <= workingDwarfs; dwarfCounter++) {
-    prompt(dwarfCounter + '. törpe a bányában van.')
-  }
-  for (dwarfCounter; dwarfCounter <= totalDwarf; dwarfCounter++) {
-    console.log(dwarfCounter + '. törpe még az ágyában alszik.')
-  }
+for (dwarfCounter; dwarfCounter <= workingDwarfs; dwarfCounter++) {
+  console.log(dwarfCounter + '. törpe a bányában van.')
+}
+for (dwarfCounter; dwarfCounter <= totalDwarf; dwarfCounter++) {
+  console.log(dwarfCounter + '. törpe még az ágyában alszik.')
 }
