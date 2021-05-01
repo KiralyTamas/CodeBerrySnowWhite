92493bfb9a8647b536b9317e6e96d205b56111b4
KiralyTamas <tomaszpest@gmail.com>
2021-05-01 17:59:10 +0200

    .js báhívása html-be folyamatban.

--- dwarf.js
+++ dwarf.js
@@ -1,9 +1,11 @@
 var totalDwarf = 10;
 var dwarfCounter = 1;
 var workingDwarfs = 7;
-for (dwarfCounter; dwarfCounter <= workingDwarfs; dwarfCounter++) {
-  console.log(dwarfCounter + '. törpe a bányában van.')
-}
-for (dwarfCounter; dwarfCounter <= totalDwarf; dwarfCounter++) {
-  console.log(dwarfCounter + '. törpe még az ágyában alszik.')
+function torpe() {
+  for (dwarfCounter; dwarfCounter <= workingDwarfs; dwarfCounter++) {
+    prompt(dwarfCounter + '. törpe a bányában van.')
+  }
+  for (dwarfCounter; dwarfCounter <= totalDwarf; dwarfCounter++) {
+    console.log(dwarfCounter + '. törpe még az ágyában alszik.')
+  }
 }
