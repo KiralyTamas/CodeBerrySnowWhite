ccaf035dded818e44e7669b89af6a441bce1d18b
KiralyTamas <tomaszpest@gmail.com>
2021-05-01 16:35:37 +0200

    életév csekkoló

---
+++ magazine.js
@@ -0,0 +1,8 @@
+let age = 1985;
+let currentYear = 2021;
+let adult = 18;
+if (adult <= currentYear - age) {
+  console.log('Köszönjük! Elolvashatja ezt a cikket');
+} else {
+  console.log('Sajnáljuk, de ez a cikk csak ' + adult + ' éven felüliek számára érjető el.')
+}
