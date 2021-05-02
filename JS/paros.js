18e5975bf4edcd88100c2acace0b8af25c2d971c
KiralyTamas <tomaszpest@gmail.com>
2021-05-02 11:16:07 +0200

    Kész a páros-páratlan figyelő függvény.

--- JS/paros.js
+++ JS/paros.js
@@ -1,6 +1,14 @@
 function oddEven(num) {
   let totalNumber = num;
   for (let numbers = 1; numbers <= totalNumber; numbers++) {
-    console.log(numbers + ' egy szám');
+    if ((numbers % 3 + numbers % 5) == 0) {
+      console.log(numbers + ' egér')
+    } else if (numbers % 3 == 0) {
+      console.log(numbers + ' kutya')
+    } else if (numbers % 5 == 0) {
+      console.log(numbers + ' cica')
+    } else {
+      console.log(numbers+' Egyikkel sem osztható')
+    }
   }
 }
