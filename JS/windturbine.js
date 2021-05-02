dd825cdc1cb1ddb95571ae92174b472cc14bc604
KiralyTamas <tomaszpest@gmail.com>
2021-05-02 16:09:12 +0200

    Kész. A szélturbinák teljesítménye számolható

--- JS/windturbine.js
+++ JS/windturbine.js
@@ -1,9 +1,13 @@
 function power(allTurbine, p1, p2, p3) {
-  for (let turbineCounter = 1; turbineCounter <= allTurbine; turbineCounter++) {
-    if (turbineCounter <= 10) {
-      console.log('A(z) ' + turbineCounter + '.számú szélturbina teljes fordulaton működik, 2000 MWh-t termelve.A farmon termelt összenergia jelenleg MWh.')
+  for (let turbineCounter = 1, sumPower = 2000; turbineCounter <= allTurbine; turbineCounter++) {
+    if (turbineCounter == 1) {
+      console.log('A(z) ' + turbineCounter + '.számú szélturbina teljes fordulaton működik, ' + p1 + ' MWh-t termelve.A farmon termelt összenergia jelenleg ' + sumPower + ' MWh.')
+    } else if (turbineCounter <= 10) {
+      console.log('A(z) ' + turbineCounter + '.számú szélturbina teljes fordulaton működik, ' + p1 + ' MWh-t termelve.A farmon termelt összenergia jelenleg ' + (sumPower = sumPower + p1) + ' MWh.')
     } else if (turbineCounter <= 15) {
-      console.log('Fing'+(sumPower=sumPower+1))
+      console.log('A(z) ' + turbineCounter + '.számú szélturbina fél fordulaton működik, ' + p2 + ' MWh-t termelve.A farmon termelt összenergia jelenleg ' + (sumPower = sumPower + p2) + ' MWh.')
+    } else if (turbineCounter <= 20) {
+      console.log('A(z) ' + turbineCounter + '. számú szélturbina nem működik, ' + p3 + ' MWh-t termelve. A farmon termelt összenergia jelenleg ' + sumPower + ' MWh.')
     }
   }
 }
