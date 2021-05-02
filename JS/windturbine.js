c44292d70a5e5a6584bd1e07d7e27d6420757002
KiralyTamas <tomaszpest@gmail.com>
2021-05-02 12:00:24 +0200

    félkész

---
+++ JS/windturbine.js
@@ -0,0 +1,9 @@
+function power(allTurbine, p1, p2, p3) {
+  for (let turbineCounter = 1; turbineCounter <= allTurbine; turbineCounter++) {
+    if (turbineCounter <= 10) {
+      console.log('A(z) ' + turbineCounter + '.számú szélturbina teljes fordulaton működik, 2000 MWh-t termelve.A farmon termelt összenergia jelenleg MWh.')
+    } else if (turbineCounter <= 15) {
+      console.log('Fing'+(sumPower=sumPower+1))
+    }
+  }
+}
