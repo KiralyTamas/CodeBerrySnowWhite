b4cdd6dfcf7bc1c6af0ed33af94d2e05a14dfe62
KiralyTamas <tomaszpest@gmail.com>
2021-05-02 20:10:09 +0200

    Tömbök javascript implementálva és töltve.

---
+++ JS/arrays.js
@@ -0,0 +1,8 @@
+let shoppingList=['kenyér','tej','alma','tojás']
+let gameOfThronesHouses = ['Stark', 'Arryn', 'Baratheon', 'Tully', 'Greyjoy', 'Lannister', 'Tyrell', 'Martell', 'Targaryen']
+let fibonacciSequence=[1,2,3,5,8,13,21,34,55,89]
+let myFavoriteMovies=['Avatar','Az-1','Az-2']
+let placesToVisit=['Olaszország','Japán','Amerika']
+shoppingList.push('rizs','tészta','bab')
+myFavoriteMovies.push('Naruto')
+placesToVisit.push('Grönland','Nagy-Britannia','Norvágia')
