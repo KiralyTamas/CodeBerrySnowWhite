3aed0eaf1f9f1c5b8ac6a0d5f4561a66abd4e0f4
KiralyTamas <tomaszpest@gmail.com>
2021-05-05 19:52:11 +0200

    Már kész is a bakancslista :)

---
+++ JS/bucketlist.js
@@ -0,0 +1,6 @@
+let bucketList=['regényt írni','megtanulni olaszul','elutazni Új-Zélandra','búvárkodni']
+bucketList.push('megnézni a sarki fényt','megtanulni gitározni','ejtőernyőzni')
+bucketList.unshift('vegetáriánussá válni','legalább 3 hónapra Franciaországba költözni')
+bucketList.pop()
+bucketList.shift()
+console.log(bucketList)
