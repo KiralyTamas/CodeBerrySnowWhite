486e0eccb78b45f609ee6120ccf0eb99906f1cdd
KiralyTamas <tomaszpest@gmail.com>
2021-05-08 09:29:53 +0200

    Objektumok elkezdve

---
+++ JS/objects.js
@@ -0,0 +1,13 @@
+let myFilm = {
+  'title': 'Avatar',
+  'author': 'James Cameron',
+  'genre': 'scifi',
+  'has been see': true,
+  'year of publication': 2008,
+  'caracters': ['Netiri', 'Jack', 'Toruk Makto']
+}
+let myFavourites = {
+  'szín': 'Kék',
+  'number': 18,
+  'Foods': ['Rakott Krumpli', 'Pörkölt', 'Spagi']
+}
