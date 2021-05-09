819932910afbc1088ddd8f3c7e12e1b44ff83c02
KiralyTamas <tomaszpest@gmail.com>
2021-05-09 11:20:43 +0200

    Most is haladtam

--- JS/objects.js
+++ JS/objects.js
@@ -4,15 +4,42 @@ let myFilm = {
   'genre': 'scifi',
   'has been see': true,
   'year of publication': 2008,
+  'borrowed by': 'Ihor',
   'main caracters': ['Netiri', 'Jack', 'Toruk Makto'],
   'quotes': {
-    'Netiri':'Látlak téged',
-    'Jack':'Egy vagyok közületek',
-    'Toruk Makto':'Repülj velem testvérem'
+    'Netiri': 'Látlak téged',
+    'Jack': 'Egy vagyok közületek',
+    'Toruk Makto': 'Repülj velem testvérem'
   }
 }
 let myFavourites = {
   'szín': 'Kék',
   'number': 18,
   'Foods': ['Rakott Krumpli', 'Pörkölt', 'Spagi']
+}
+var solarSystem = {
+  'Mercury': 'bolygó',
+  'Venus': 'bolygó',
+  'Earth': 'bolygó',
+  'Mars': 'bolygó',
+  'Jupiter': 'bolygó',
+  'Saturn': 'bolygó',
+  'Uranus': 'bolygó',
+  'Neptune': 'bolygó',
+  'Pluto': 'bolygó',
+  'Ceres': 'törpebolygó',
+  'Makemake': 'törpebolygó',
+  'Haumea': 'törpebolygó',
+  'Eris': 'törpebolygó'
+}
+solarSystem.Pluto = 'törpebolygó'
+myFilm.rating = 5;
+var apartment = {
+  'type of the building': 'tégla',
+  'floor': '4. emelet',
+  'number of rooms': '3 szoba',
+  'size': '62 négyzetméter',
+  'view': 'kert',
+  'pets allowed': ['kutyák', 'macskák'],
+  'shortest rental period': 'legalább egy év'
 }
